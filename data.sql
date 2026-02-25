175932f5805cbe6739c1abd151fd28abc01c98ad
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2026-02-25 09:24:51 +0700

    Commit: 4

--- data.sql
+++ data.sql
@@ -10,3 +10,16 @@ CREATE TABLE IF NOT EXISTS plants (
 );
 
 
+CREATE TABLE IF NOT EXISTS watches (
+                                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(100) NOT NULL,
+    path_gambar VARCHAR(255) NOT NULL,
+    deskripsi TEXT NOT NULL,
+    price VARCHAR(50) NOT NULL,
+    specs TEXT NOT NULL,
+    features TEXT NOT NULL,
+    material TEXT NOT NULL,
+    battery_life VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
